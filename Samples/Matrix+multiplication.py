46f5f32ed2ada86dcd3e8b2cbd62f01ffe0cc426
MAC <mac@ACooper-Mac.local>
2017-12-19 21:24:10 +0800

    矩阵乘法

--- Samples/Matrix+multiplication.py
+++ Samples/Matrix+multiplication.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python
+#
+#
+#
+
 import numpy as np
+
+matrixA = np.arange(100)
+matrixA.shape = (10,10)
+print matrixA
+
+matrixB = np.arange(100)
+matrixB.shape = (10,10)
+print matrixB
+
+matrixR = matrixA*matrixB
+print matrixR
