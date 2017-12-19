4d312ed08c079ba0dc3a4294f3fbdc7630bee7aa
MAC <mac@ACooper-Mac.local>
2017-12-19 20:37:42 +0800

    rm readme

--- PERSONALNOTE.md
+++ PERSONALNOTE.md
@@ -10,3 +10,11 @@
 2.ndim:返回数组的维数;<br/>
 3.itemsize:返回数组中每个元素的字节单位长度;<br/>
 4.flags:返回了数组的属性值;<br/>
+5.empty和dtype:产生一个指定行列数的随机矩阵，dtype指定元素类型;<br/>
+6.zeros:返回特定大小，以 0 填充的新数组,zeros((5,6));<br/>
+7.ones:返回特定大小，以 1 填充的新数组,ones((2,3));<br/>
+
+#### 来自现有数据的数组
+1.asarray:将Python序列转换为ndarray,例如:asarray(序列);<br/>
+2.frombuffer:将缓冲区解释为一维数组,例如:frombuffer(字符串, dtype = 'S1');<br/>
+3.
--- numpy_ndarray+attribute.py
+++ numpy_ndarray+attribute.py
@@ -23,3 +23,33 @@ print d.itemsize
 #get flags of ndarray
 e = np.array([1,2,3,4,5])
 print e.flags
+
+#the empty of ndarray
+f = np.empty([4,6], dtype = int)
+print f
+
+#the zeros of ndarray
+g = np.zeros((5,6), dtype = int)
+print g
+
+#the ones of ndarray
+h = np.ones((2,3), dtype = float)
+print h
+
+#-----------------
+#
+# ndarray data from there
+#
+#-----------------
+
+#asarray
+x = [1,2,3]
+A = np.asarray(x, dtype = int)
+print A
+
+#frombuffer
+tmp = 'Hello Man'
+B = np.frombuffer(tmp, dtype = 'S1')
+print B
+
+#fromiter
